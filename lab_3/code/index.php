bd4d90b4768be25c8c960c264192ea5c8f302991
chandrianin <TbMA39@yandex.ru>
2024-03-23 16:53:36 +0200

    add task 1

--- lab_3/code/index.php
+++ lab_3/code/index.php
@@ -1,24 +1,47 @@
 <?php
 
-
-/**README:
- *Каждый пункт обозначен следующей записью
- *<p>// ПУНКТ №n</p>
- *<p>Между собой пункты разделены двумя пустыми строками,
- * задания внутри пунктов разделены одной пустой строкой</p>
- *<p>Названия многих переменных изменены по сравнению с <a href="https://docs.google.com/document/d/1SBLXePwd46nyNNLZ7AuzUBiQirLzBkPEqmIMu_y_xq4/edit">заданием</a></p>
- *<p>Для оптимального просмотра стоит использовать браузер</p>
- */
-
-
 // подключаю свой шрифт для браузера
 echo '<style>@font-face {
-    font-family: "googleSans";
-    src: url("font/GoogleSans-Regular.ttf") format("ttf");
+font-family: "googleSans";
+src: url("font/GoogleSans-Regular.ttf") format("ttf");
 }
-* {
+ * {
 font-family: "googleSans", sans-serif;
+font-size: 23px;
+}
+h2 {
+    margin-left: 5px;
+} 
+h3 {
+margin-left: 20px;
+}
+p {
+margin-left: 30px;
 }
 </style > ';
 
 
+/**<h2>1. Регулярные выражения</h2>*/
+
+/**<h3>Подпункт а)</h3>*/
+echo '<h2>1. Регулярные выражения</h2>';
+$stringForSplit = 'ahb acb aeb aeeb adcb axeb';
+$regularExpression = '/a[a-z]{2}b/';
+$results = array();
+preg_match_all($regularExpression, $stringForSplit, $results);
+echo '<h3>а)</h3>';
+for ($i = 0; $i < sizeof($results[0]); $i++) {
+    echo "<p>{$results[0][$i]}</p>";
+}
+
+/**<h3>Подпункт б)</h3>*/
+echo '<h3>б)</h3>';
+$stringForSplit = 'a1b2c3';
+$regularExpression = '/([a-z])([0-9])/';
+$results = array();
+$resultString = preg_replace_callback($regularExpression,
+    function ($matches) {
+        return $matches[1] . pow(intval($matches[2]), 3);
+    },
+    $stringForSplit);
+echo "<p>$resultString</p>";
