a4f4a9c6cc6ea516b97a71aaa1c584ccafdd4c04
chandrianin <TbMA39@yandex.ru>
2024-03-23 13:55:39 +0200

    add base files

---
+++ lab_3/code/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**README:
+ *Каждый пункт обозначен следующей записью
+ *<p>// ПУНКТ №n</p>
+ *<p>Между собой пункты разделены двумя пустыми строками,
+ * задания внутри пунктов разделены одной пустой строкой</p>
+ *<p>Названия многих переменных изменены по сравнению с <a href="https://docs.google.com/document/d/1SBLXePwd46nyNNLZ7AuzUBiQirLzBkPEqmIMu_y_xq4/edit">заданием</a></p>
+ *<p>Для оптимального просмотра стоит использовать браузер</p>
+ */
+
+
+// подключаю свой шрифт для браузера
+echo '<style>@font-face {
+    font-family: "googleSans";
+    src: url("font/GoogleSans-Regular.ttf") format("ttf");
+}
+* {
+font-family: "googleSans", sans-serif;
+}
+</style > ';
+
+
